ce8d31d037016b553a0d063c8eee6749d4fae775
Rivaldo Ieong <gaprivaldo@gmail.com>
2019-06-09 08:33:06 +0800

    Initial Commit

---
+++ UDDER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UDDER
+//
+//  Created by Rivaldo Ieong on 2019/6/9.
+//  Copyright © 2019 Rivaldo Ieong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
